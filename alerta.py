13d230a3583f5b06528329bffe260dee82920353
Elilúcio <eliluciofilho@gmail.com>
2025-11-17 23:23:54 -0300

    adicionar docstring explicativa para alerta

--- alerta.py
+++ alerta.py
@@ -1,2 +1,3 @@
 class alerta:
+    """Sistema de alertas para quando certos critérios forem alcançados, como um valor de despesa acima de R$500 antes de seu cadastro e de saldo mensal negativo caso chegue a tal ponto."""
     pass
