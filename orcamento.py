63494d96487ed7bd33d6018e9f97a193b82993fe
Elilúcio <eliluciofilho@gmail.com>
2025-11-17 23:04:19 -0300

    mover docstring para dentro da classe

--- orcamento.py
+++ orcamento.py
@@ -1,4 +1,6 @@
 class orcamento:
+    """Calcula e apresenta o saldo mensal e diário, além de também calcular as despesas e receitas totais, alerta quando o saldo mensal for negativo."""
     pass
 
-"""Calcula e apresenta o saldo mensal e diário, além de também calcular as despesas e receitas totais, alerta quando o saldo mensal for negativo."""
+
+
