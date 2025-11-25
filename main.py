d627adf196387c81d8cf073f7f0123affff09052
Elilúcio <eliluciofilho@gmail.com>
2025-11-25 14:53:39 -0300

    Implement category management in main.py

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-class Main:
-    """Onde todo o código irá convergir e funcionar"""
-    pass
+from modules.cad_categ import cad_categ
 
+#criando objetos
+despesa = cad_categ()
+despesa.cadastrar()
+receita = cad_categ()
+receita.cadastrar()
+
+#vendo os objetos criados
+despesa.ver_categ()
+receita.ver_categ()
+
+#editando objetos
+despesa.update_categ()
+receita.update_categ()
+
+#"deletando" objetos
+despesa.del_categ()
+receita.del_categ()
+
+#vendo os objetos deletados causa erro
+try:
+    receita.ver_categ()
+except AttributeError:
+    print("Receita deletada com sucesso!")
